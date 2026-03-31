646c79a2dd67ceeacb658882d7bbf77db4cc2dda
Raj patel <9795093341raj@gmail.com>
2026-03-31 17:42:44 +0530

    added header and body

--- app.js
+++ app.js
@@ -1,19 +1,91 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
 
+/*
+*Header
+    -logo
+    -nav items
+*Body
+    -search
+    -restaurantcontainer
+    -restaurantcard
+        .name of res , rating ,cuisine, delivery time
+*Footer
+    -copyright
+    -links
+    -address
+    -contact
+*/
 
-const parent = React.createElement("div",{id:"parent"},
-    [
-        React.createElement("div",{id:"child"},
-            [React.createElement("h1",{id:"heading"},"I am heading h1"),
-                React.createElement("h2",{id:"heading"},"I am heading h2"),
-            ]
-        ),
-        React.createElement("div",{id:"child2"},
-            [React.createElement("h1",{id:"heading"},"I am heading h1"),
-                React.createElement("h2",{id:"heading"},"I am heading h2"),
-            ]
-        )
-    ]
-)
+const Header=()=>{
+    return(
+        <div className="header">
+            <div className="logo-conainer">
+                <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const styleCard ={
+    backgroundColor: "#f0f0f0",
+}
+
+const RestaurantCard=()=>{
+    return(
+        <div className="res-card" style={styleCard}>
+            <img  className="res-logo"
+            alt="res-logo"
+            src= "https://static.vecteezy.com/system/resources/previews/040/986/112/non_2x/ai-generated-indian-biryani-rice-professional-advertising-foodgraphy-photo.jpg"
+            />
+            <h3>Four Season</h3>
+            <h4>North Indian ,Biryani ,Asian</h4>
+            <h4>4.2 stars</h4>
+            <h4>28 minutes</h4>
+        </div>
+    )
+}
+
+const Body=()=>{
+    return(
+        <div className="body">
+            <div className="search">
+                Search
+            </div>
+            <div className="res-container">
+
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+
+            </div>
+        </div>
+    )
+}
+
+const Applayout=()=>{
+    return(
+        <div className="app"> 
+            <Header/>
+            <Body/>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<Applayout/>);
+
+
