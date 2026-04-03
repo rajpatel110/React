782a9c9912e22767b9995891eb4637c0dce92465
Raj patel <9795093341raj@gmail.com>
2026-04-03 16:37:43 +0530

    fetched restaurant data from swiggy api using useEffect

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -20,7 +20,7 @@ return (
         cloudinaryImageId
         }
     />
-    <h3>{name}</h3>
+    <h4>{name}</h4>
     <h4>{cuisines.join(", ")}</h4>
     <h4>{avgRating} stars</h4>
     <h4>{costForTwo}</h4>
